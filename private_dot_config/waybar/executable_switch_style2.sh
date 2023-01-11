9d7c1fa7facdd2a046ec906235be2f0409f43bc2
xiyini <q2z@hotmail.com>
2023-01-11 07:52:30 +0400

    delete all

--- private_dot_config/waybar/executable_switch_style2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-killall waybar
-SDIR="$HOME/.config/waybar"
-waybar -c "$SDIR"/config2 -s "$SDIR"/style2.css &
-
--- private_dot_config/waybar/executable_switch_style3.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-killall waybar
-SDIR="$HOME/.config/waybar"
-waybar -c "$SDIR"/config3 -s "$SDIR"/style3.css &
-
