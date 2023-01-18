107caec3531a8ae0c3c74eb365680775b6484a96
xiyini <q2z@hotmail.com>
2023-01-18 08:38:35 +0400

    Update .config/lvim/config.lua

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
--- lvim.colorscheme = "base16-tokyodark-terminal"
+lvim.colorscheme = "base16-gruvbox-dark-hard"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
