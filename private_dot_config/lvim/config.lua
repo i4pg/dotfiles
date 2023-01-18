605bca0d770f82206ae728bb4ef0825e605af579
xiyini <q2z@hotmail.com>
2023-01-18 11:45:34 +0400

    Update README.md Add assest/image_afternoon.png Add assest/morning.png Update .config/lvim/config.lua

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
-lvim.colorscheme = "wal"
+-- lvim.colorscheme = "wal"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
