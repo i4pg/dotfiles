0139be876d74c54a95e30a821b6e31685fe3c6e0
xiyini <q2z@hotmail.com>
2023-01-18 11:32:30 +0400

    Update .config/lvim/config.lua

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -320,7 +320,7 @@ vim.opt.smartindent = true -- make indenting smarter again
 vim.opt.splitbelow = true -- force all horizontal splits to go below current window
 vim.opt.splitright = true -- force all vertical splits to go to the right of current window
 vim.opt.swapfile = true -- creates a swapfile
--- vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
+vim.opt.termguicolors = true -- set term gui colors (most terminals support this)
 vim.opt.timeoutlen = 100 -- time to wait for a mapped sequence to complete (in milliseconds)
 vim.opt.title = true -- set the title of window to the value of the titlestring
 vim.opt.titlestring = "%<%F%=%l/%L - nvim" -- what the title of the window will be set to
