ae22e44143c786d22145c8de039030b6e7dd5091
i4pg <86368009+i4pg@users.noreply.github.com>
2023-05-24 08:48:22 +0400

    Update .config/lvim/config.lua

--- private_dot_config/lvim/config.lua
+++ private_dot_config/lvim/config.lua
@@ -11,7 +11,7 @@ an executable
 -- general
 lvim.log.level = "warn"
 lvim.format_on_save.enabled = true
-lvim.colorscheme = "kanagawa"
+lvim.colorscheme = "catppuccin"
 -- to disable icons and use a minimalist setup, uncomment the following
 -- lvim.use_icons = false
 
