e21891ef74478b42624eb23b246284251342d988
i4pg <86368009+i4pg@users.noreply.github.com>
2023-09-07 14:53:45 +0400

    Update .config/waybar/to_latte.sh Update .config/waybar/to_mocha.sh

--- private_dot_config/waybar/executable_to_latte.sh
+++ private_dot_config/waybar/executable_to_latte.sh
@@ -2,4 +2,6 @@
 killall waybar
 SDIR="$HOME/.config/waybar"
 waybar -c "$SDIR"/latte.jsonc -s "$SDIR"/latte.css &
+killall swaybg
+swaybg -i ~/Downloads/latte-noise4-wallhaven-5gwmd3_3840x2160.png
 
--- private_dot_config/waybar/executable_to_mocha.sh
+++ private_dot_config/waybar/executable_to_mocha.sh
@@ -2,4 +2,6 @@
 killall waybar
 SDIR="$HOME/.config/waybar"
 waybar -c "$SDIR"/mocha.jsonc -s "$SDIR"/mocha.css &
+killall swaybg
+swaybg -i ~/Downloads/mocha-noise4-wallhaven-5gwmd3_3840x2160.png
 
